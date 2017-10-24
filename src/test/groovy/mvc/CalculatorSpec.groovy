97ac5c414e937df3d2197893d588ddea64fb4a99
Manuel Stutz <manuel.stutz@students.fhnw.ch>
2017-10-24 12:48:32 +0200

    Updated

---
+++ src/test/groovy/mvc/CalculatorSpec.groovy
@@ -0,0 +1,37 @@
+package mvc
+
+import geb.spock.GebSpec
+import grails.test.mixin.integration.Integration
+import spock.lang.Unroll
+
+/**
+ * See http://www.gebish.org/manual/current/ for more instructions
+ */
+@Integration
+class CalculatorSpec extends GebSpec {
+
+    @Unroll
+    void "Basic calculation: with en #enInput and exam #examInput should give result #result"() {
+        when:
+            go '/static/GradeCalculator.html'
+        then:
+        	title == "Grade Calculator"
+
+        when: "set valid input"
+            $("form").en   = enInput
+            $("form").exam = examInput
+            $("input", type: "submit").click()
+
+        then: "Result Page is displayed"
+            title == "Average"
+            $("output").text() == result
+
+        where:
+            enInput | examInput | result
+            4.0     |5.0        | "4.5"
+            6.0     |5.0        | "5.5"
+            3.0     |5.0        | "4.0"
+            5.0     |5.0        | "5.0"
+            6.0     |6.0        | "6.0"
+    }
+}
